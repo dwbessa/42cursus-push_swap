89ac2d790770a6225e6a1f14b460c246ffeb6e2d
dwbessa <danielbessa01@gmail.com>
2024-01-07 10:29:51 -0300

    fix: conditional in main func]

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 10:40:14 by dbessa            #+#    #+#             */
-/*   Updated: 2024/01/06 23:25:17 by dbessa           ###   ########.fr       */
+/*   Updated: 2024/01/06 23:28:25 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,21 @@ int	main(int ac, char **av)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	if (ac > 1)
+	check = 0;
+	if (ac == 1 || (ac == 2 && !av[1][0]))
+		exit(EXIT_FAILURE);
+	else if (ac == 2)
+		av = ft_split(av[1], ' ');
+	size_list = split_len(av);
+	if (ac > 2)
 	{
-		check = 0;
-		if (ac == 1 || (ac == 2 && !av[1][0]))
-			return (1);
-		else if (ac == 2)
-			av = ft_split(av[1], ' ');
-		size_list = split_len(av);
-		if (ac > 2)
-		{
-			size_list--;
-			check = 1;
-		}
-		stack_a = create_stack(av, size_list, check);
-		stack_b = create_stack(av, 0, check);
-		use_sort(stack_x, stack_y, size_list);
-		free(stack_a);
-		free(stack_b);
-		return (0);
+		size_list--;
+		check = 1;
 	}
-	exit(EXIT_FAILURE);
+	stack_a = create_stack(av, size_list, check);
+	stack_b = create_stack(av, 0, check);
+	use_sort(stack_x, stack_y, size_list);
+	free(stack_a);
+	free(stack_b);
+	return (0);
 }
