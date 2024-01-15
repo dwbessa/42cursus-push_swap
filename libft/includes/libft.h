d808a7d85047ba5402562bd013d37d2f3511b075
dwbessa <danielbessa01@gmail.com>
2024-01-15 16:39:22 -0300

    feat: limits.h in lib

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 16:20:59 by dbessa            #+#    #+#             */
-/*   Updated: 2023/11/23 11:31:34 by dbessa           ###   ########.fr       */
+/*   Updated: 2024/01/15 15:18:52 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 # include "ft_printf.h"
 # include "get_next_line.h"
 
@@ -58,6 +59,7 @@ char			*ft_strrchr(const char *s, int c);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_atoi(const char *nptr);
+long			ft_atol(const char *nptr);
 
 void			*ft_memset(void *s, int c, size_t n);
 void			ft_bzero(void *s, size_t n);
